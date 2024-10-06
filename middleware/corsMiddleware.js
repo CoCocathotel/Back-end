d78df9607936c0b4d3d086525d4795ccf244198e
Maoswagger <adisak.2457@gmail.com>
2024-10-06 18:34:29 +0700

    1sadasdก

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -11,7 +11,11 @@ const corsMiddleware = cors(corsOptions);
 // Handling preflight requests for all routes
 const handlePreflight = (req, res, next) => {
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');  
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     console.log('Received an OPTIONS request');
     return res.sendStatus(200); // Always respond with status 200 for OPTIONS
   }
