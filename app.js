c53a40ed7c6743abb1b57398393cd0b596432759
CoCocathotel <cococat872024@gmail.com>
2024-11-16 01:11:31 +0700

    tset

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+const cors = require("cors");
 const compression = require("compression");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -18,32 +19,21 @@ const app = express();
 
 app.use(compression());
 
-// Middleware to set CORS headers manually
-app.use((req, res, next) => {
-  const allowedOrigins = ['https://cococatbackend.vercel.app', 'http://localhost:3000'];
-  const origin = req.headers.origin;
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ["Content-Type", "Authorization", "X-Access-Token"]
+};
 
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Access-Token");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 
 app.use(require('./router/router'));
+
 
 app.get("/*", (req, res) => {
   res.send("Welcome to the API");
